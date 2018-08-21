96c9590bdabb67bde215666169f640a0c754ac4a
kevin.margueritte <kevin.margueritte@tabmo.io>
2018-08-21 11:35:26 +0200

    Adding README.md :pencil:

--- extra-rules/src/main/scala/io.tabmo.circe.extra.rules/Test.scala
+++ extra-rules/src/main/scala/io.tabmo.circe.extra.rules/Test.scala
@@ -1,5 +1,7 @@
 package io.tabmo.circe.extra.rules
 
+import java.util.Date
+
 import io.circe.Decoder.Result
 import io.circe.syntax._
 import io.circe.{Decoder, HCursor, Json}
@@ -8,12 +10,14 @@ object Test extends App {
 
   import io.tabmo.json.rules._
 
-  case class Person(firstName: String, lastName: String, list: Seq[Int])
+  case class Person(firstName: String, lastName: String, age: Int, email: String, dateOfBirth: Date)
 
   val personJson = Json.obj(
-    "name" -> "toto".asJson,
-    "lastName" -> "titi".asJson,
-    "list" -> ("1" :: "2" :: "3" :: Nil).asJson
+    "name" -> "Kevin".asJson,
+    "lastName" -> "Mg".asJson,
+    "age" -> 24.asJson,
+    "email" -> "email@email.com".asJson,
+    "dateOfBirth" -> "1994-02-16".asJson
   )
 
   val personJson2 = Json.obj(
@@ -25,10 +29,12 @@ object Test extends App {
   val decodePerson: Decoder[Person] = new Decoder[Person] {
     override def apply(c: HCursor): Result[Person] = {
       for {
-        name <- c.downField("name").read(StringRules.maxLength(8))
-        lastName <- c.downField("lastName").as[String]
-        list <- c.downField("list").readSeq(IntRules.max(3) |+| IntRules.max(1))
-      } yield Person(name, lastName, list)
+        name        <- c.downField("name").read(StringRules.maxLength(32))
+        lastName    <- c.downField("lastName").as[String]
+        age         <- c.downField("age").read(IntRules.positive())
+        email       <- c.downField("email").read(StringRules.email)
+        dateOfBirth <- c.downField("dateOfBirth").read(DateRules.date)
+      } yield Person(name, lastName, age, email, dateOfBirth)
     }
   }
 
